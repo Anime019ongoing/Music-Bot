37455503f0ed1f7ca234ccee5880b553f0a80ba0
Anime019ongoing <rioviooob@gmail.com>
2025-05-05 12:01:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID = int(os.getenv("20793620"))
+API_HASH = os.getenv("a712d2b8486f26c4dee5127cc9ae0615")
+BOT_TOKEN = os.getenv("7453023748:AAFnDpI7g5KjxqaICqQqqkSRQJCAawUc-cE")
